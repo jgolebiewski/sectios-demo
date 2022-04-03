436ca6293b7c38fc938d9fda694aaede9a20d2a0
jgolebiewski <janusz@golebiewski.info.pl>
2022-04-03 12:46:03 +0200

    moved sections init

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
 import React, { useState } from 'react';
 import { Report } from './components/Report/Report';
 import { ReportModel } from './domain/ReportModel';
-import { SectionModel } from './domain/SectionModel';
 import { AppWrapper } from './App.styled';
-import { SectionFieldModel } from './domain/SectionFieldModel';
 import { ThemeProvider, initializeIcons, setIconOptions } from '@fluentui/react';
 
 const reportModel = new ReportModel();
 reportModel.name = "Some Report";
-reportModel.sections = [
-  new SectionModel({
-    name: 'Section 1', fields: [
-      new SectionFieldModel({ name: 'Field 1', value: 'some value' }),
-      new SectionFieldModel({ name: 'Field 2', value: '' }),
-    ]
-  }),
-  new SectionModel({
-    name: 'Section 2', fields: [
-      new SectionFieldModel({ name: 'Field 3', value: '' }),
-      new SectionFieldModel({ name: 'Field 4', value: '' }),
-    ]
-  }),
-  new SectionModel({
-    name: 'Section 3', fields: [
-      new SectionFieldModel({ name: 'Field 5', value: '' }),
-      new SectionFieldModel({ name: 'Field 6', value: '' })
-    ]
-  }),
-];
 
 
 function App() {
--- src/domain/ReportModel.ts
+++ src/domain/ReportModel.ts
@@ -2,13 +2,33 @@ import { SectionModel } from "./SectionModel";
 import { v4 as uuidv4 } from 'uuid';
 import { OverviewModel } from './OverviewModel';
 import { RouteModel } from './RouteModel';
+import { SectionFieldModel } from "./SectionFieldModel";
 export class ReportModel {
 
     id: string = uuidv4();
 
     name: string = '';
 
-    sections: SectionModel[] = [];
+    sections: SectionModel[] = [
+        new SectionModel({
+            name: 'Section 1', fields: [
+                new SectionFieldModel({ name: 'Field 1', value: 'some value' }),
+                new SectionFieldModel({ name: 'Field 2', value: '' }),
+            ]
+        }),
+        new SectionModel({
+            name: 'Section 2', fields: [
+                new SectionFieldModel({ name: 'Field 3', value: '' }),
+                new SectionFieldModel({ name: 'Field 4', value: '' }),
+            ]
+        }),
+        new SectionModel({
+            name: 'Section 3', fields: [
+                new SectionFieldModel({ name: 'Field 5', value: '' }),
+                new SectionFieldModel({ name: 'Field 6', value: '' })
+            ]
+        }),
+    ];
 
     overview: OverviewModel = new OverviewModel();
 
