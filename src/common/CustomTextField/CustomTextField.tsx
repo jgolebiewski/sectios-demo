df80cbc3179ac45462b368a0bad8fd3a5224a237
Janusz Gołębiewski <janusz@golebiewski.info.pl>
2022-05-04 18:09:57 +0200

    test lint

--- src/common/CustomTextField/CustomTextField.tsx
+++ src/common/CustomTextField/CustomTextField.tsx
@@ -1,16 +1,25 @@
-import { TextField } from '@fluentui/react';
+import { ActionButton, IIconProps, TextField } from '@fluentui/react';
 import { FC } from 'react';
 import { Controller } from 'react-hook-form';
 import { CustomFieldProps } from '../../domain/CustomFieldProps';
 
+const removeIcon: IIconProps = { iconName: 'Remove' };
+
 export const CustomTextField: FC<CustomFieldProps> = (props): JSX.Element => {
     const { name, label, control, disabled } = props;
 
+    const handleRemove = (d) => {
+        console.log('remove dee');
+    };
+
     return (
-        <Controller
-            name={name}
-            control={control}
-            render={({ field }) => <TextField {...field} label={label} disabled={disabled} />}
-        />
+        <div>
+            <ActionButton iconProps={removeIcon} text="Remove field" onClick={handleRemove} />
+            <Controller
+                name={name}
+                control={control}
+                render={({ field }) => <TextField {...field} label={label} disabled={disabled} />}
+            />
+        </div>
     );
 };
